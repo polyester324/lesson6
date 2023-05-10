7701b4e7a892b96ea42ed3bb18c8a62350243571
polina <polinakozlova324@gmail.com>
2023-05-10 21:05:22 +0300

    task1

---
+++ src/practice/task1/Calculator.java
@@ -0,0 +1,19 @@
+package practice.task1;
+
+public class Calculator {
+    double weight = 10.5;
+    int cost = 100;
+    String color = "red";
+    long findSum(int a, int b){
+        return a + b;
+    }
+    int findRazn(int  a, int b){
+        return a - b;
+    }
+    double findDel(int a, int b){
+        return (double) a / b;
+    }
+    double findMult(int a, int b){
+        return a * b;
+    }
+}
